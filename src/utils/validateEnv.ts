31752db03dca9ae533382dd6d7df384c6329de03
sansx <646924078@qq.com>
2024-05-13 14:17:52 +0800

    update config

--- src/utils/validateEnv.ts
+++ src/utils/validateEnv.ts
@@ -4,7 +4,7 @@ import { logHandler } from "./logHandler";
  * Quick module to confirm environment values are present.
  */
 export const validateEnv = () => {
-  if (!process.env.CROWDIN_API) {
+  if (!process.env.CROWDIN_API || !process.env.DEEPL_KEY) {
     logHandler.log("error", "Missing API key!");
     process.exit(1);
   }
